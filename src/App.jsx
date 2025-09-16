66b255c49ce6e76a59ac594e5575dddd2dc465df
mrjaini <aj230074@gmail.com>
2025-09-16 15:29:25 +0530

    path changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Sidebar } from './components/layout/Sidebar';
-import { Header } from './components/layout/Header';
+import { Sidebar } from './components/Layout/Sidebar';
+import { Header } from './components/Layout/Header';
 import { DashboardPage } from './pages/DashboardPage';
 
 function App() {
